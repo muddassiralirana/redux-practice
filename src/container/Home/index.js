0371f5ba9e6e093f4ae29bb8bcf0b65e5b6868c1
muddassirAliRana <muddassiralirana786@gmail.com>
2022-04-24 19:21:55 +0500

    add repo

---
+++ src/container/Home/index.js
@@ -0,0 +1,25 @@
+import { useSelector, useDispatch } from 'react-redux'
+import { getDataFromApi } from '../../store/Actions'
+import { Slider, Product } from '../../components'
+import './css/index.css'
+function Home() {
+    const store = useSelector(state => state.auth);
+    const dispatch = useDispatch()
+    // console.log(store)
+
+    return (
+        <div>
+            <Slider />
+            <div className="products">
+                <div>
+                    <h4>Products</h4>
+                    <div className="border_line">
+                    </div>
+                </div>
+                <Product />
+            </div>
+        </div>
+    )
+}
+
+export default Home;
